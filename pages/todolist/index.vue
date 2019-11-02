b11d8063f479ed17618b66dc60ccdc84c3db9d54
Bruno Braz <bruno_natanael_braz@hotmail.com>
2019-11-02 15:35:29 -0300

    architeture

---
+++ pages/todolist/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1 v-if="todos.length < 0">todolist</h1>
+    <ul v-else>
+        <li v-for="todo in todos" v-bind:key="todo._id">{{todo.description}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todos: []
+        }
+    },
+
+    mounted(){
+        this.$axios.$get('items')
+        .then(() => {
+            //this.todos = response
+        })
+        .catch((e) => {
+            console.error(e)
+        })
+    }
+}
+</script>
