01737eb971fc00ec266c66a8834339ac9a4b41e3
Bruno Braz <bruno_natanael_braz@hotmail.com>
2019-11-02 18:41:05 -0300

    update fail

--- pages/todolist/index.js
+++ pages/todolist/index.js
@@ -2,12 +2,6 @@ import BootcampInput from '~/components/bootcamp-input/bootcamp-input.vue';
 import BootcampList from '~/components/bootcamp-list/bootcamp-list.vue';
 
 export default {
-  data() {
-    return {
-      i: 1,
-      itemsList: [],
-    }
-  },
   /**
    * registra os componentes
    */
@@ -15,17 +9,21 @@ export default {
     BootcampInput,
     BootcampList,
   },
+  mounted(){
+    this.$store.dispatch('todos/feachTodos')
+  },
+  computed: {
+    itemsList(){
+      return this.$store.getters['todos/TODOS']
+    }
+  },
   methods: {
     /**
      * adiciona item na list
      * recebe valor vindo do componente filho
      */
     addItemList(value) {
-      this.itemsList.push({
-        id: this.i++,
-        description: value,
-        checked: '1'
-      });
+      this.$store.dispatch('todo/addTodo', value)
     },
     /**
      * recebe valor vindo do componente
@@ -43,11 +41,7 @@ export default {
      * conforme action faz alteração na lista
      */
     deleteItemList(value) {
-      this.itemsList.map((i, k) => {
-        if (i._id == value.item._id) {
-          this.itemsList.splice(k, 1)
-        }
-      })
+      this.$store.dispatch('todos/removeTodo', value.item)
     },
     /**
      * muda estado de item na lista
